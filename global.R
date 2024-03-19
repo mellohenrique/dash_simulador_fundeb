271e931556d77b582a39258b4217b786f6d8650d
Henrique de Mello Santos de Assuncao <henrique.assuncao@sesicni.com.br>
2024-03-19 09:58:40 -0300

    Adiciona ggplot2 aos pacotes usados

--- global.R
+++ global.R
@@ -7,6 +7,7 @@ library("shinydashboard")
 library("shinydashboardPlus")
 library("markdown")
 library("simulador.fundeb")
+library("ggplot2")
 options(scipen=999)
 
 # Carrega dados
@@ -15,3 +16,4 @@ load("data/alunos.rda")
 load("data/complementos.rda")
 load("data/simulacao_inicial.rda")
 load("data/simulacao_inicial_agregada.rda")
+load('data/mapa.rda')
